a56be6112abc38d23d4ca86e98ed4fb92a75075a
Nightrou <devrou4@gmail.com>
2025-01-18 19:04:09 +0200

    feat: shelf and item update view

--- shelfs/urls.py
+++ shelfs/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import ShelfCreateView, ItemCreateView, ItemDetailView
+from .views import ShelfCreateView, ShelfUpdateView, ItemCreateView, ItemUpdateView, ItemDetailView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='shelfr-home'),
     path('shelf/<int:pk>/', views.shelf, name='shelfr-shelf'),
     path('shelf/new/', ShelfCreateView.as_view(), name='shelfr-shelf-new'),
+    path('shelf/<int:pk>/update/', ShelfUpdateView.as_view(), name='shelfr-shelf-update'),
     path('shelf/<int:pk>/new/', ItemCreateView.as_view(), name='shelfr-item-new'),
     path('shelf/<int:shelf_pk>/<int:pk>/', ItemDetailView.as_view(), name='shelfr-item-detail'),
+    path('shelf/<int:shelf_pk>/<int:pk>/update/', ItemUpdateView.as_view(), name='shelfr-item-update'),
 ]
--- shelfs/views.py
+++ shelfs/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Shelf, Item
 
 # Create your views here.
@@ -27,6 +27,23 @@ class ShelfCreateView(LoginRequiredMixin, CreateView):
     
     def get_success_url(self):
         return reverse('shelfr-home')
+    
+class ShelfUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Shelf
+    fields = ['title', 'content', 'image']
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('shelfr-home')
+    
+    def test_func(self):
+        shelf = self.get_object()
+        if self.request.user == shelf.owner:
+            return True
+        return False
 
 class ItemCreateView(LoginRequiredMixin, CreateView):
     model = Item
@@ -40,6 +57,26 @@ class ItemCreateView(LoginRequiredMixin, CreateView):
     
     def get_success_url(self):
         return reverse('shelfr-shelf', kwargs={'pk': self.kwargs['pk']})
+    
+class ItemUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Item
+    fields = ['title', 'content', 'image', 'quantity']
+
+    def form_valid(self, form):
+        shelf = get_object_or_404(Shelf, pk=self.kwargs['shelf_pk'])
+        form.instance.shelf = shelf
+
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        item = self.get_object()
+        return reverse('shelfr-item-detail', kwargs={'shelf_pk': item.shelf.id, 'pk': item.id})
+    
+    def test_func(self):
+        item = self.get_object()
+        if self.request.user == item.shelf.owner:
+            return True
+        return False
 
 class ItemDetailView(LoginRequiredMixin, DetailView):
     model = Item
