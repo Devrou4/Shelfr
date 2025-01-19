35fbc58e9056170b8ef415acd115e51926a60177
Nightrou <devrou4@gmail.com>
2025-01-19 19:08:46 +0200

    feat: responsive navbar and user update view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm
 
 # Create your views here.
 def register(request):
@@ -16,3 +17,18 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {"form":form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Account updated.')
+            return redirect('profile')
+
+    else:
+        form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'users/profile.html', {'form': form})
